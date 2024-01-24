163afa58c2d0f1602862e90081b1751b51468205
TSB <122724396+tsbproject@users.noreply.github.com>
2024-01-24 01:46:37 -0800

    [committed]

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+$(document).ready(function () {
+    var previousScroll = 0;
+    var $navbar = $('.custom-navbar');
+    var $logo = $('.navbar-logo');
+    var $navLinks = $('.nav-link');
+
+    $(window).scroll(function () {
+        var currentScroll = $(this).scrollTop();
+
+        if (currentScroll > previousScroll) {
+            // Scrolling down
+            $navbar.css('height', '60px');
+            $navbar.css('padding', '10px 0');
+            $logo.css('max-height', '40px');
+            $navLinks.addClass('shrink');
+        } else {
+            // Scrolling up
+            $navbar.css('height', '80px');
+            $navbar.css('padding', '20px 0');
+            $logo.css('max-height', '50px');
+            $navLinks.removeClass('shrink');
+        }
+
+        previousScroll = currentScroll;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 // FORM VALIDATON
 function validateForm() {
     var name = document.getElementById('name').value;
